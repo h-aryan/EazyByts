aa93f0cdb867b9159fd0551481659824e68e8397
Harsh <haryan998@gmail.com>
2025-06-12 16:15:52 +0530

    setup

--- portfolio-backend/app.js
+++ portfolio-backend/app.js
@@ -6,7 +6,9 @@ const userRoutes = require("../portfolio-backend/route/userRoute");
 const app = express();
 
 app.use(express.json()); // Parse JSON bodies
-app.use(cors());
+app.use(
+  cors({ origin: "https://your-frontend.netlify.app", credentials: true })
+);
 app.use(morgan("dev"));
 app.use("/api/user", userRoutes);
 
