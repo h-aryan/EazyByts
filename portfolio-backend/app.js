af75a2c85a64ea5b0f650b42dd65c7a991377681
Harsh <haryan998@gmail.com>
2025-06-12 17:45:36 +0530

    setup

--- portfolio-backend/app.js
+++ portfolio-backend/app.js
@@ -6,9 +6,20 @@ const userRoutes = require("../portfolio-backend/route/userRoute");
 const app = express();
 
 app.use(express.json()); // Parse JSON bodies
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://phenomenal-horse-e7f24a.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000", // or 3000, whichever port you're using for React
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
